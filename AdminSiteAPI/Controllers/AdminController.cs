ac90f51873f6b388393e9a7031cc630d5df4273b
rana271 <ranacomp@gmail.com>
2023-05-31 11:28:25 +0530

    Add project files.

---
+++ AdminSiteAPI/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using AdminSiteAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+namespace AdminSiteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        public List<User> Index()
+        {
+            List<User> users = new List<User>();
+            //users.Add(new User { })
+            SqlConnection con = new SqlConnection(@"Data Source=RANA\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users", con);
+            SqlDataReader dr= cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                users.Add(new User { Id = dr["id"].ToString(), 
+                    UserName = dr["username"].ToString(), 
+                    CompanyID = dr["CompanyID"].ToString(), 
+                    CompanyName = dr["CompanyName"].ToString(), 
+                    UserType = dr["usertype"].ToString() });
+            }
+            return users;
+
+        }
+    }
+}
---
+++ AdminSiteAPI/Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AdminSiteAPI.Model
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+
+        public string CompanyID { get; set; }
+        public string CompanyName { get; set; }
+        public string UserType { get; set; }
+
+    }
+}
